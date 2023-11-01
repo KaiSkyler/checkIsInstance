bb7518842019a59d7445a9280dffd0a2560cb375
Kaya <741299544@qq.com>
2023-11-01 14:56:24 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-//检查给定的值是否是给定类或超类的实例
+//检查给定的值是否是给定类或超类的实例 通过instanceof实现
 function checkIsInstance(val, classFun){
     if(classFun == '' || !classFun || val == '' || !val) return false;
     return Object(val) instanceof classFun;
 }
 
-() => checkIsInstance(new Date(), Date);
-() => checkIsInstance(Date, Date);
-() => { class Animal {}; class Dog extends Animal {}; return checkIsInstance(new Dog(), Animal); };
-() => checkIfInstanceOf(5, Number);
+//通过原型链迭代查询
+function checkIsInstanceByProto(val, classFun) {
+    if(classFun == '' || !classFun || val == '' || !val) return false;
+    while(val){
+        if(val.__proto__ === classFun.prototype) return true;
+        val = val.__proto__;
+    }
+}
+
+// () => checkIsInstance(new Date(), Date);
+// () => checkIsInstance(Date, Date);
+// () => { class Animal {}; class Dog extends Animal {}; return checkIsInstance(new Dog(), Animal); };
+// () => checkIfInstanceOf(5, Number);
+
