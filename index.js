eb3b4e8ca58ef11d9e54e5eb144ad9e161fbcfca
Kaya <741299544@qq.com>
2023-11-01 15:24:40 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 //检查给定的值是否是给定类或超类的实例 通过instanceof实现
-function checkIsInstance(val, classFun){
-    if(classFun == '' || !classFun || val == '' || !val) return false;
-    return Object(val) instanceof classFun;
-}
+function checkIsInstance(obj, classFunction) {
+  if (obj === null || obj === undefined || !(classFunction instanceof Function))
+    return false;
+  return Object(obj) instanceof classFunction;
+};
+
 
 //通过原型链迭代查询
 function checkIsInstanceByProto(val, classFun) {
